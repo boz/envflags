474084ef95ffdcb9c1a987ceb265a5d15bd9df04
Adam Bozanich <adam.boz@gmail.com>
2019-07-29 09:19:04 -0700

    testing

--- envflags_test.go
+++ envflags_test.go
@@ -11,13 +11,18 @@ func Test_ApplyWith(t *testing.T) {
 	env := []string{
 		"ENVFLAGS_TEST_FOO=value",
 		"ENVFLAGS_TEST_BAR=100",
+		"ENVFLAGS_TEST_QUUX=do-not-want",
 	}
 
 	flags := pflag.NewFlagSet("cli", pflag.ContinueOnError)
+
 	flags.String("foo", "", "the foo")
 	flags.Int("bar", 10, "the bar")
+	flags.String("quux", "", "the quux")
 
-	assert.NoError(t, flags.Parse([]string{}))
+	assert.NoError(t, flags.Parse([]string{
+		"--quux", "quux-want",
+	}))
 	assert.NoError(t, ApplyWith(env, flags))
 
 	{
@@ -30,5 +35,39 @@ func Test_ApplyWith(t *testing.T) {
 		val, err := flags.GetInt("bar")
 		assert.NoError(t, err)
 		assert.Equal(t, 100, val)
+	}
+
+	{
+		val, err := flags.GetString("quux")
+		assert.NoError(t, err)
+		assert.Equal(t, "quux-want", val)
+	}
+}
+
+func Test_defaultTransformFn(t *testing.T) {
+	tests := [][]string{
+		{"ab", "AB"},
+		{"a-b", "A_B"},
+		{"a.b", "A_B"},
+		{"a b", "AB"},
+		{"a/b", "AB"},
+		{"", ""},
+		{"~", ""},
+		{"_", "_"},
+		{"-", "_"},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test[1], defaultTransformFn(test[0]), test[0])
+	}
+}
+
+func Test_defaultNameFn(t *testing.T) {
+	tests := [][]string{
+		{"a", "b", "a_b"},
+		{"", "b", "b"},
+		{"a", "", ""},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test[2], defaultNameFn(test[0], test[1]), test[0], test[1])
 	}
 }
